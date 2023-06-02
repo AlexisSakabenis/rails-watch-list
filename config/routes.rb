dce004bea62a7443862eb5d4f61f1337c53ae451
Alexis Sakabenis <alexis.10@outlook.be>
2023-06-02 14:40:41 +0200

    road

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :articles
+  resources :articles, except: :index
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -11,9 +11,4 @@ Rails.application.routes.draw do
   end
 
   root 'movies#index'
-end
-
-Rails.application.routes.draw do
-  root to: "articles#index"
-  resources :articles, except: :index
 end
